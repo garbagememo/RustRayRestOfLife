f07109d5b98e2f3d77e574532e801dfe3502044e
garbagememo <66831242+garbagememo@users.noreply.github.com>
2025-11-09 21:15:52 +0900

    シーンファイルの持ち方を改稿中

--- src/main.rs
+++ src/main.rs
@@ -8,48 +8,6 @@ use rayon::prelude::*;
 use std::io::Write;
 
 
-fn ray_color(r: &Ray, world: &dyn Shape, depth: i64, background: Vec3) -> Vec3 {
-     if depth <= 0 {
-        return Vec3::new(0.0, 0.0, 0.0);
-     }
-    let black=Box::new(ColorTexture::new(Vec3::zero()));
-    let hit_info = world.hit(&r, EPS, f64::MAX);
-    if let Some(hit) = hit_info {
-        let emitted = hit.m.emitted(&r, &hit);
-        let scatter_info = hit.m.scatter(r, &hit);
-        if let Some(scatter) = scatter_info {
-            if let Some(pdf)=scatter.pdf {
-                let pdf = MixturePdf::new(
-                    Arc::new(ShapePdf::new(
-                        Box::new(Rect::new(
-                            213.0, 343.0, 227.0, 332.0, 554.0,RectAxisType::XZ,
-                            Arc::new(Lambertian::new(black))
-                        )),hit.p,)
-                    ),
-                    Arc::clone(&pdf) );
-                let new_ray = Ray::new(hit.p,pdf.generate(&hit));
-                let spdf_value = pdf.value(&hit,new_ray.d);
-                if spdf_value > 0.0 {
-                    let pdf_value = hit.m.scattering_pdf(&new_ray, &hit);
-                    let albedo = scatter.albedo * pdf_value;
-                    emitted
-                        + albedo.mult(ray_color(&new_ray, world, depth-1, background)) /spdf_value
-
-                } else {
-                    emitted
-                }
-            }else{
-                emitted
-                    + scatter.albedo.mult(ray_color(&scatter.ray, world, depth-1, background)) 
-            }
-        } else {
-            emitted
-        }
-    } else { // hit=None
-        return background;
-    }
-}
-
 fn main() {
     let args = parameters();
     println!("{:?}", args);
@@ -65,53 +23,14 @@ fn main() {
     let mut world = ShapeList::new();
     let mut background =Vec3::new(0.7,0.8,1.0);
     //オリジナルはRayの関数だがとりあえず定数で
-    let cam: Camera;
-    match args.m {
-        0 => {//デフォルトはゼロ
-            w = args.w;
-            h = ((w as f64) / SQUARE_ASPECT) as usize;
-     	    background=Vec3::new(0.0,0.0,0.0);
-            cam = world.cornell_mirror_box_scene();
-        }
-        1 => {
-            cam = world.simple_scene();
-	    }
-        2 => {
-            cam = world.random_scene();
-        }
-        3 => {
-            cam = world.texture_scene();
-        }
-        4 => {
-     	    background=Vec3::new(0.1,0.1,0.1);
-            cam = world.emitte_scene();
-        }
-        5 =>{
-     	    background=Vec3::new(0.1,0.1,0.1);
-            cam = world.emitte_squre_scene();
-        }
-        6 => {
-            w = args.w;
-            h = ((w as f64) / SQUARE_ASPECT) as usize;
-            cam = world.cornellbox_scene();
-            background=Vec3::zero();
-        }
-        7 =>{
-            w = args.w;
-            h = ((w as f64) / SQUARE_ASPECT) as usize;
-            cam = world.cornell_scene();
-            background=Vec3::zero();
-        }
-        8 =>{
-                        w = args.w;
-            h = ((w as f64) / SQUARE_ASPECT) as usize;
-            cam = world.cornell_mirror_box_scene();
-            background=Vec3::zero();
-        }
-        _ => {
-            cam = world.simple_scene();
-        }
-    }
+
+    let mut Cornell=CornellBoxScene::new();
+    let cam =Cornell.cam;
+    
+    w = args.w;
+    h = ((w as f64) / SQUARE_ASPECT) as usize;
+    background=Vec3::new(0.0,0.0,0.0);
+    let cam = world.cornell_mirror_box_scene();
 
     let mut image = vec![Color::zero(); (w * h) as usize];
     let bands: Vec<(usize, &mut [Color])> = image.chunks_mut(w as usize).enumerate().collect();
@@ -123,8 +42,8 @@ fn main() {
                     for _sx in 0..2 {
                         let u = (x as f64 + (_sx as f64 + random()) / 4.0) / (w as f64);
                         let v = (y as f64 + (_sy as f64 + random()) / 4.0) / (h as f64);
-                        let ray = cam.get_ray(u, v);
-                        r = r + ray_color(&ray, &world, MAX_DEPTH, background)
+                        let ray = Cornell.cam.get_ray(u, v);
+                        r = r + Cornell.ray_color(&ray,MAX_DEPTH, background)
                             / (samps as f64)
                             / 4.0;
                     }
